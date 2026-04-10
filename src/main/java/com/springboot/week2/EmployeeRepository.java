9d900da5baefbdc797d2c1dfc31b167f9183c5c8
auspiciouskunal <kkunal0207@gmail.com>
2026-04-10 20:23:38 +0530

    week 2.2 || Starting with Repository and Entity

---
+++ src/main/java/com/springboot/week2/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.week2;
+
+import com.springboot.week2.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
+}
---
+++ src/main/java/com/springboot/week2/entity/EmployeeEntity.java
@@ -0,0 +1,27 @@
+package com.springboot.week2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="employee")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer age;
+    private String name;
+    private String emailId;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
+}
