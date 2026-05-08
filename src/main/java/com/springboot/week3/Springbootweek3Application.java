15eedbbeebb7bd9043d3083187bf28b64c27da30
auspiciouskunal <kkunal0207@gmail.com>
2026-05-08 14:34:26 +0530

    Initial commit for week 3

---
+++ src/main/java/com/springboot/week3/Springbootweek3Application.java
@@ -0,0 +1,13 @@
+package com.springboot.week3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootweek3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootweek3Application.class, args);
+	}
+
+}
